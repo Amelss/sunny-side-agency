d1452dfb7f4d2078a0b1cf7fd03e7a32465646c3
Amelss <ameleyka@gmail.com>
2022-03-17 19:49:56 +0000

    tailwind setup

--- sunny-side/src/App.js
+++ sunny-side/src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-    <h1>hello</h1>
+    <h1 className="font-bold text-xs text-softYellow">hello</h1>
     </div>
   );
 }
---
+++ sunny-side/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#ffffff",
+        grayBlue: "#a7abae",
+        darkGrayBlue: "#818498",
+        grayishBlue: "#5a636c",
+        desBlue: "#23303e",
+        modCyan: "#458c7e",
+        darkBlue: "#19536b",
+        desCyan: "#25564b",
+        softYellow: "#fad400",
+        softRed: "#fe7867",
+      },
+    },
+  },
+  plugins: [],
+};
